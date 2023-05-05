d2e11e5e9977422b1c96eadbd5049649acc92bbc
Oleksandr Dubin <102242242+AlexDubin@users.noreply.github.com>
2023-05-05 18:42:01 +0200

    Изменения в файле index.html

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,18 +2,36 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
+
 const startBtn = document.querySelector('[data-start]');
 const daysData = document.querySelector('[data-days]');
 const hoursData = document.querySelector('[data-hours]');
 const minutesData = document.querySelector('[data-minutes]');
 const secondsData = document.querySelector('[data-seconds]');
 
-const currentDate = new Date();
 const dateInput = document.querySelector('#datetime-picker');
 
-let intervId = null;
+let intervId;
 
-startBtn.setAttribute('disabled', 'disabled');
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose: dateInputClose,
+};
+
+flatpickr(dateInput, options);
+
+function dateInputClose(selectedDates) {
+  const selectedDate = selectedDates[0];
+  if (selectedDate < new Date()) {
+    Notiflix.Notify.warning('Please choose a date in the future');
+    startBtn.disabled = true;
+  } else {
+    startBtn.disabled = false;
+  }
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -34,42 +52,51 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-function addLeadingZero(value) {
-  return value.toString().padStart(2, '0');
+function startBtnClick() {
+  clearInterval(intervId);
+
+  const currentDate = new Date(dateInput.value).getTime();
+  const now = new Date().getTime();
+
+  if (isNaN(currentDate) || currentDate < now) {
+    Notiflix.Notify.failure('Please choose a valid date in the future');
+    return;
+  }
+
+  updateTimer();
+
+  intervId = setInterval(() => {
+    updateTimer();
+  }, 1000);
 }
 
-const option = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    currentDate.getTime();
-    selectedDates[0].getTime();
-    if (currentDate > selectedDates[0]) {
-      return Notiflix.Report.warning(
-        'Error',
-        'Please choose a date in the future!',
-        'Ok'
-      );
-    }
-    startBtn.removeAttribute('disabled');
-    startBtn.addEventListener('click', () => {
-      intervId = setInterval(() => {
-        let time = convertMs(
-          selectedDates[0].getTime() - Date.parse(new Date())
-        );
-        daysData.innerHTML = addLeadingZero(time.days);
-        hoursData.innerHTML = addLeadingZero(time.hours);
-        minutesData.innerHTML = addLeadingZero(time.minutes);
-        secondsData.innerHTML = addLeadingZero(time.seconds);
-        if (selectedDates[0].getTime() - Date.parse(new Date()) <= 0) {
-          clearInterval(intervId);
-        }
-      }, 1000);
-    });
-  },
-};
+function updateTimer() {
+  const currentDate = new Date(dateInput.value).getTime();
+  const now = new Date().getTime();
 
-flatpickr(dateInput, option);
+  if (isNaN(currentDate) || currentDate < now) {
+    Notiflix.Notify.failure('Please choose a valid date in the future');
+    clearInterval(intervId);
+    return;
+  }
+
+  let timeLeft = currentDate - now;
+
+  if (timeLeft < 0) {
+    clearInterval(intervId);
+    daysData.textContent = '00';
+    hoursData.textContent = '00';
+    minutesData.textContent = '00';
+    secondsData.textContent = '00';
+    startBtn.disabled = true;
+    Notiflix.Notify.success('Countdown finished!');
+  } else {
+    const timeObject = convertMs(timeLeft);
+    daysData.textContent = timeObject.days.toString().padStart(2, '0');
+    hoursData.textContent = timeObject.hours.toString().padStart(2, '0');
+    minutesData.textContent = timeObject.minutes.toString().padStart(2, '0');
+    secondsData.textContent = timeObject.seconds.toString().padStart(2, '0');
+  }
+}
+
+startBtn.addEventListener('click', startBtnClick);
